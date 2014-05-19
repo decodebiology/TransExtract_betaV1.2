1d6bc3b39f57b52d53e015a3e6f20e232e017154
Santhilal Subhash <santhilalsubhash@gmail.com>
2014-05-19 16:42:27 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -8,33 +8,3 @@
 outpath='' ## The folder where the output should be stored
 inpath='' ## The folder which contains all input VCF files
 samples=("SAMPLE-1" "SAMPLE-2") ## Input your sample names: VCF file should be named as SAMPLE-1_variations.vcf, SAMPLE-2_variations.vcf...
-strand=("FR" "RR") 
-for i in "${samples[@]}"
-do
-	for j in "${strand[@]}"
-	do
-		mkdir -p $outpath/$i/$j
-	done
-done
-
-
-
-
-
-
-BaseQual=5 ## Your baseQuality cutoff
-readDepth=2 ## Your ReadDepth cutoff
-for i in "${samples[@]}"
-do
-VCF="$inpath/${i}_variations.vcf"
-
-for j in "${strand[@]}"
-do
-
-perl Transitions_extraction.pl $BaseQual $i $readDepth $j $VCF $outpath
-done
-done
-
-echo 'script v1.2'
-echo 'Author: Santhilal Subhash'
-echo 'Contact: santhilal.subhash@gu.se'
